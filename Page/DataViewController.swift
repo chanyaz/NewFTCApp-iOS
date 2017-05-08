4daa5dc7b770d2e42c4cb27660c83e82210cf429
Oliver Zhang <[oli.zhang@gmail.com]>
2017-05-08 08:40:01 +0800

    Initial Commit

---
+++ Page/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Page
+//
+//  Created by Oliver Zhang on 2017/5/8.
+//  Copyright © 2017年 Oliver Zhang. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+

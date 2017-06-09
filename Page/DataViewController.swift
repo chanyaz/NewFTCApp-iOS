9cc1a0851e68dc6fb6d633905610b374fee83e3f
Oliver Zhang <[oli.zhang@gmail.com]>
2017-06-09 17:10:41 +0800

    download

--- Page/DataViewController.swift
+++ Page/DataViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 private let reuseIdentifier = "ItemCell"
 
 class DataViewController: UICollectionViewController {
-
+    
     
     // MARK: - Once The dataObject is changed, UI should be updated
     var dataObject = [String: String]()
@@ -24,22 +24,23 @@ class DataViewController: UICollectionViewController {
     }
     
     
-    func getDataFromUrl(_ url:URL, completion: @escaping ((_ data: Data?, _ response: URLResponse?, _ error: NSError? ) -> Void)) {
-        let listTask = URLSession.shared.dataTask(with: url, completionHandler:{(data, response, error) in
-            completion(data, response, error as NSError?)
-            return ()
-        })
-        listTask.resume()
-    }
+
     
     private func getAPI(_ urlString: String) {
         let url = URL(string: urlString)
         if let urlValue = url {
-            getDataFromUrl(urlValue) { (data, response, error)  in
+            Download().getDataFromUrl(urlValue) { (data, response, error)  in
                 DispatchQueue.main.async { () -> Void in
                     guard let data = data , error == nil else { return }
-                    print (data)
-                    //self.handleJSONData(data)
+                    do {
+                        let JSON = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions(rawValue: 0))
+                        if let json = JSON as? [String: Any] {
+                            print ("data retrieved")
+                            self.pageContent = json
+                        }
+                    } catch {
+                        
+                    }
                 }
             }
         }
@@ -60,13 +61,13 @@ class DataViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Register cell classes
         // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
+        
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(hex: AppNavigation.sharedInstance.defaultContentBackgroundColor)
         
@@ -74,79 +75,79 @@ class DataViewController: UICollectionViewController {
         requestNewContent()
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
     }
-
-
+    
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return 100
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         // Configure the cell
         switch reuseIdentifier {
-            case "ItemCell":
-                if let cell = cell as? ItemCell {
-                    cell.title.text = "headline of the story"
-                    cell.lead.text = "lead for this story"
-             return cell
+        case "ItemCell":
+            if let cell = cell as? ItemCell {
+                cell.title.text = "headline of the story"
+                cell.lead.text = "lead for this story"
+                return cell
             }
         default: break
         }
-    
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
+    // MARK: UICollectionViewDelegate
+    
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
+    
 }
---
+++ Page/Download.swift
@@ -0,0 +1,18 @@
+//
+//  Download.swift
+//  Page
+//
+//  Created by Oliver Zhang on 2017/6/9.
+//  Copyright © 2017年 Oliver Zhang. All rights reserved.
+//
+
+import Foundation
+struct Download {
+    public func getDataFromUrl(_ url:URL, completion: @escaping ((_ data: Data?, _ response: URLResponse?, _ error: NSError? ) -> Void)) {
+        let listTask = URLSession.shared.dataTask(with: url, completionHandler:{(data, response, error) in
+            completion(data, response, error as NSError?)
+            return ()
+        })
+        listTask.resume()
+    }
+}

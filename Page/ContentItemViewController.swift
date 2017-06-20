923ac861d18808f9f95f8d033d5555c55bc2d24a
Oliver Zhang <[oli.zhang@gmail.com]>
2017-06-20 15:32:16 +0800

    story page

--- Page/ContentItemViewController.swift
+++ Page/ContentItemViewController.swift
@@ -21,9 +21,59 @@ class ContentItemViewController: UIViewController {
     var pageTitle: String = ""
     var themeColor: String?
     
-    @IBOutlet weak var headline: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var toolBar: UIToolbar!
 
     override func viewDidLoad() {
-        headline.text = dataObject?.headline
+        super.viewDidLoad()
+        initStyle()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        initText()
+        
+    }
+    
+    func initStyle() {
+        textView.backgroundColor = UIColor(hex: AppNavigation.sharedInstance.defaultContentBackgroundColor)
+        toolBar.backgroundColor = UIColor(hex: AppNavigation.sharedInstance.defaultTabBackgroundColor)
+        toolBar.barTintColor = UIColor(hex: AppNavigation.sharedInstance.defaultTabBackgroundColor)
+        toolBar.isTranslucent = false
+        //toolBar.barStyle = .black
+        
+        
+    }
+    
+    func initText() {
+        let finalText = NSMutableAttributedString()
+    
+        // MARK: headline
+        let headlineStyle = NSMutableParagraphStyle()
+        headlineStyle.paragraphSpacing = 12.0
+        let headlineString = dataObject?.headline ?? ""
+        let headline = NSAttributedString(string: "\(headlineString)\n", attributes: [NSParagraphStyleAttributeName: headlineStyle])
+        
+        
+        // MARK: body
+        let bodyStyle = NSMutableParagraphStyle()
+        bodyStyle.paragraphSpacing = 12.0
+        let bodyString = dataObject?.lead ?? ""
+        let body = NSAttributedString(string: bodyString, attributes: [NSParagraphStyleAttributeName: bodyStyle])
+        
+        // MARK: the final text
+        finalText.append(headline)
+        finalText.append(body)
+        
+        textView.attributedText = finalText
+        
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        
+        // MARK: - a workaround for the myterious scroll view bug
+        textView.isScrollEnabled = false
+        textView.isScrollEnabled = true
+        textView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
+    }
+    
+    
 }

12ab181b7d7d13e70dbf7fd39cb3cf31c6f5a8e5
Oliver Zhang <oli.zhang@gmail.com>
2017-06-23 10:01:22 +0800

    convert html tag into nsattributedstring

--- Page/ContentItemViewController.swift
+++ Page/ContentItemViewController.swift
@@ -97,7 +97,8 @@ class ContentItemViewController: UIViewController {
         // MARK: https://makeapppie.com/2016/07/05/using-attributed-strings-in-swift-3-0/
         // MARK: Convert HTML to NSMutableAttributedString https://stackoverflow.com/questions/36427442/nsfontattributename-not-applied-to-nsattributedstring
         
-        let finalText = NSMutableAttributedString()
+        
+        let text = NSMutableAttributedString()
         
         let headlineStyle = NSMutableParagraphStyle()
         headlineStyle.paragraphSpacing = 12.0
@@ -111,19 +112,36 @@ class ContentItemViewController: UIViewController {
             ]
         )
         
-        let bodyAttributes:[String:AnyObject]? = [
+        let bodyAttributes:[String:AnyObject] = [
             NSFontAttributeName:UIFont.preferredFont(forTextStyle: .body),
             NSForegroundColorAttributeName:UIColor.blue
         ]
-        let cBodyString = dataObject?.cbody ?? dataObject?.lead ?? "body"
-        let cbody = NSAttributedString(string: cBodyString, attributes: bodyAttributes)
-
-        finalText.append(headline)
-        finalText.append(cbody)
+        
+        let bodyString = dataObject?.cbody ?? dataObject?.lead ?? "body"
+        //let cbody = NSAttributedString(string: cBodyString, attributes: bodyAttributes)
+        let body: NSMutableAttributedString
+        
+        let cbodyTest = bodyString.htmlToAttributedString()
+        
+        if let htmlBody = bodyString.htmlAttributedString() {
+            body = htmlBody
+        } else {
+            body = NSMutableAttributedString(string: bodyString, attributes: nil)
+        }
+        body.addAttributes(bodyAttributes, range: NSMakeRange(0, body.length))
+        
+        
+        
+        
+        text.append(headline)
+        text.append(body)
         //Apply to the label
-        textView?.attributedText = finalText
+        textView?.attributedText = text
         
-        
+        // MARK: - a workaround for the myterious scroll view bug
+        textView?.isScrollEnabled = false
+        textView?.isScrollEnabled = true
+        textView?.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
         
         
         
@@ -189,4 +207,21 @@ class ContentItemViewController: UIViewController {
          */
     }
     
+    
+}
+
+extension String {
+    func htmlAttributedString() -> NSMutableAttributedString? {
+        guard let data = self.data(using: String.Encoding.utf16, allowLossyConversion: false) else { return nil }
+        guard let html = try? NSMutableAttributedString(
+            data: data,
+            options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+            documentAttributes: nil) else { return nil }
+        return html
+    }
+    func htmlToAttributedString() -> NSMutableAttributedString? {
+        let text = self.replacingOccurrences(of: "<[pP]>", with: "\n", options: .regularExpression)
+        print (text)
+        return nil
+    }
 }

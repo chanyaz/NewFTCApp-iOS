d9cdc4bb09cb0642a79b6cee95e6551c6d6ce3d3
Oliver Zhang <[oli.zhang@gmail.com]>
2017-09-04 09:20:43 +0800

    constants

--- APIs.swift
+++ APIs.swift
@@ -1,68 +1,13 @@
 //
-//  Style.swift
+//  File.swift
 //  Page
 //
-//  Created by Oliver Zhang on 2017/6/22.
+//  Created by Oliver Zhang on 2017/9/4.
 //  Copyright © 2017年 Oliver Zhang. All rights reserved.
 //
 
 import Foundation
 import UIKit
-struct Color {
-    // Those nested structures are grouped mostly according to their functions
-    struct Content {
-        static let headline = "#000000"
-        static let body = "#333333"
-        static let lead = "#777777"
-        static let border = "#e9decf"
-        static let background = "#FFF1E0"
-        static let tag = "#9E2F50"
-        static let time = "#8b572a"
-    }
-    
-    struct Tab {
-        static let text = "#333333"
-        static let normalText = "#555555"
-        static let highlightedText = "#c0282e"
-        static let border = "#d4c9bc"
-        static let background = "#f7e9d8"
-    }
-    
-    struct Button {
-        static let tint = "#057b93"
-    }
-    
-    struct Header {
-        static let text = "#333333"
-    }
-    
-    struct ChannelScroller {
-        static let text = "#565656"
-        static let highlightedText = "#c0282c"
-        static let background = "#fff9f5"
-        //static let background = "#e8dbcb"
-        
-        //static let background = "#FFFFFF"
-        
-    }
-    
-    struct Navigation {
-        static let border = "#d5c6b3"
-    }
-    
-    struct Ad {
-        static let background = "#f6e9d8"
-        static let sign = "#555555"
-        static let signBackground = "#ecd4b4"
-    }
-    
-}
-
-struct FontSize {
-    static let bodyExtraSize: CGFloat = 3.0
-    static let padding: CGFloat = 14
-}
-
 // MARK: Different organization might use different way to construct API and urls
 struct APIs {
     //private static let base = "https://m.ftimg.net/index.php/jsapi/"
@@ -170,33 +115,8 @@ struct Event {
     }
 }
 
-struct GA {
-    static let trackingIds = ["UA-1608715-1", "UA-1608715-3"]
-}
-
-struct WeChat {
-    // MARK: - wechat developer appid
-    static let appId = "wxc1bc20ee7478536a"
-    static let appSecret = "14999fe35546acc84ecdddab197ed0fd"
-    static let accessTokenPrefix = "https://api.weixin.qq.com/sns/oauth2/access_token?"
-    static let userInfoPrefix = "https://api.weixin.qq.com/sns/userinfo?"
-}
-
-struct Share {
-    static let base = "http://www.ftchinese.com/"
-    static let shareIconName = "ShareIcon.jpg"
-    struct CampaignCode {
-        static let wechat = "2G178002"
-        static let actionsheet = "iosaction"
-    }
-}
-
 struct Push {
     static let deviceTokenUrl = "https://noti.ftimg.net/iphone-collect.php"
-}
-
-struct AppGroup {
-    static let name = "group.com.ft.ftchinese.mobile"
 }
 
 struct Key {

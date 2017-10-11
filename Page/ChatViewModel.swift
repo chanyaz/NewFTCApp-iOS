70cee9d7fd76bf150ccc3b3100490817f1173c83
wangyichen1064431086 <1064431086@qq.com>
2017-10-11 18:49:19 +0800

    Let the width of bubble match with device. But it is leading to some new little bugs

--- Page/ChatViewModel.swift
+++ Page/ChatViewModel.swift
@@ -115,6 +115,7 @@ class CellData {
     var cutlineColor = UIColor(hex: "#999999")
     let getMoreHistoryColor = UIColor(hex: "#999999")
     //基本尺寸
+    let bubbleShorterLen = CGFloat(20)
     var bubbleImageInsets = UIEdgeInsetsMake(8, 20, 10, 12)//文字嵌入气泡的边距
     var bubbleStrechInsets = UIEdgeInsetsMake(18.5, 24, 18.5, 18.5)//气泡点九拉伸时的边距
     var cellInsets = UIEdgeInsetsMake(10, 5, 15, 5)//头像嵌入Cell的最小边距
@@ -122,14 +123,13 @@ class CellData {
     var headImageLength = CGFloat(50) //正方形头像边长
     var betweenHeadAndBubble = CGFloat(5) //头像和气泡的左右距离
     
-    var maxTextWidth = CGFloat(240)//文字最大宽度
+    var maxTextWidth = CGFloat(240)//文字最大宽度---TODO:修改为根据设备宽度计算
     var maxTextHeight = CGFloat(10000.0) //文字最大高度
-    var defaultImageWidth = CGFloat(240)//图片消息还未获取到图片数据时默认图片宽度
-    var defaultImageHeight = CGFloat(135)//图片消息还未获取到图片数据时默认图片高度
-    //var maxImageWidth = CGFloat(200) //图像消息的图片最大宽度
-    //var maxImageHeight = CGFloat(400) //图像消息的图片最大高度
-    var coverWidth = CGFloat(240)//TODO:待修改，因为会超出iPhone 5的边界
-    var coverHeight = CGFloat(135)//Cover图像统一是16*19的，这里统一为240*135
+    var defaultImageWidth = CGFloat(240)//图片消息还未获取到图片数据时默认图片宽度--TODO:修改为根据设备宽度计算
+    var defaultImageHeight = CGFloat(135)//图片消息还未获取到图片数据时默认图片高度--TODO:修改为根据设备宽度计算
+   
+    var coverWidth = CGFloat(240)//TODO:修改为根据设备宽度计算
+    var coverHeight = CGFloat(135)//Cover图像统一是16*19的，这里统一为240*135--TODO:修改为根据设备宽度计算
     let cutlineCellHeight = CGFloat(50)
     let getMoreHistoryHeight = CGFloat(50)
     
@@ -158,12 +158,14 @@ class CellData {
             return self.headImageLength + cellInsets.top + cellInsets.bottom //60
         }
     }
-    var cellHeightByBubble: CGFloat {
+    /*
+    var cellHeightByBubble:CGFloat {
         get {
             return self.bubbleImageHeight + bubbleInsets.top + bubbleInsets.bottom
         }
     }
-    
+   */
+    var cellHeightByBubble = CGFloat(0)//--TODO:要在Cell里面更新
     // 一些必须在数据里生成的和view相关的对象
     var strechedBubbleImage = UIImage()
 
@@ -238,46 +240,46 @@ class CellData {
     
     //创建Text类型数据:
      func buildTextCellData(textContent text: String) {//wycNOTE: mutating func:可以在mutating方法中修改结构体属性
-        //let font = UIFont.systemFont(ofSize:18)
-        //self.normalFont = font
-        let atts = [NSAttributedStringKey.font: self.normalFont]
-        let saysWhatNSString = text as NSString
-        
-        let size = saysWhatNSString.boundingRect(
-            with: CGSize(width:self.maxTextWidth, height:self.maxTextHeight),
-            options: .usesLineFragmentOrigin,
-            attributes: atts,
-            context: nil)
-        let computeWidth = max(size.size.width,20) //修正计算错误
-        /* QUEST:boundingRect为什么不能直接得到正确结果？而且为什么
-         * 已解决：因为此处的font大小和实际font大小不同，只有为UILabelView设置属性font为一样的UIFont对象，才能保证大小合适
-         * 另说明：此处当文字多余一行时，自动就是宽度固定为最大宽度，高度自适应
+        /*
+            let atts = [NSAttributedStringKey.font: self.normalFont]
+            let saysWhatNSString = text as NSString
+         
+            let size = saysWhatNSString.boundingRect(
+                with: CGSize(width:self.maxTextWidth, height:self.maxTextHeight),
+                options: .usesLineFragmentOrigin,
+                attributes: atts,
+                context: nil)
+            let computeWidth = max(size.size.width,20) //修正计算错误
+            /* QUEST:boundingRect为什么不能直接得到正确结果？而且为什么
+             * 已解决：因为此处的font大小和实际font大小不同，只有为UILabelView设置属性font为一样的UIFont对象，才能保证大小合适
+             * 另说明：此处当文字多余一行时，自动就是宽度固定为最大宽度，高度自适应
+             */
+            let computeHeight = size.size.height
+         
+         
+            self.bubbleImageWidth = computeWidth + bubbleImageInsets.left + bubbleImageInsets.right
+            self.bubbleImageHeight = computeHeight + bubbleImageInsets.top + bubbleImageInsets.bottom
+         
+            self.saysWhatWidth = computeWidth
+            self.saysWhatHeight = computeHeight
          */
-        let computeHeight = size.size.height
-        
-        
-        self.bubbleImageWidth = computeWidth + bubbleImageInsets.left + bubbleImageInsets.right
-        self.bubbleImageHeight = computeHeight + bubbleImageInsets.top + bubbleImageInsets.bottom
-        
-        self.saysWhatWidth = computeWidth
-        self.saysWhatHeight = computeHeight
     }
     
 
     //创建Image类型数据:
     
      func buildImageCellData() {
-        
-        self.bubbleImageWidth = self.defaultImageWidth + bubbleImageInsets.left + bubbleImageInsets.right
-        self.bubbleImageHeight = self.defaultImageHeight + bubbleImageInsets.top + bubbleImageInsets.bottom
+        /*
+            self.bubbleImageWidth = self.defaultImageWidth + bubbleImageInsets.left + bubbleImageInsets.right
+            self.bubbleImageHeight = self.defaultImageHeight + bubbleImageInsets.top + bubbleImageInsets.bottom
+         */
     }
     
     
     //创建Card类型数据:
      func buildCardCellData(title titleStr: String, coverUrl coverUrlStr: String, description descriptionStr:String) {
         //处理title
-       // let titleFont = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.bold)
-        //self.titleFont = titleFont
+        /*
         let atts = [NSAttributedStringKey.font: self.titleFont]
         let titleNSString = titleStr as NSString
         let size = titleNSString.boundingRect(
@@ -287,13 +289,13 @@ class CellData {
             context: nil)
         self.titleWidth = 240
         self.titleHeight = size.size.height
-
+         */
         //处理cover:交给另一个线程asyncBuildImage处理
         
         //处理description
+        /*
         if (descriptionStr != "") {
-            //let descriptionFont = UIFont.systemFont(ofSize:18)
-            //self.descriptionFont = descriptionFont
+            
             let descriptionAtts = [NSAttributedStringKey.font: self.descriptionFont]
             let descriptionNSString = descriptionStr as NSString
             
@@ -305,13 +307,15 @@ class CellData {
             self.descriptionWidth = 240
             self.descriptionHeight = descriptionSize.size.height
         }
-        
+        */
         
         //处理总bubble
+        /*
         self.saysWhatWidth = self.coverWidth
         self.saysWhatHeight = self.titleHeight + self.coverHeight + self.descriptionHeight
         self.bubbleImageWidth = self.saysWhatWidth + self.bubbleImageInsets.left + self.bubbleImageInsets.right
         self.bubbleImageHeight = self.saysWhatHeight + self.bubbleImageInsets.top + self.bubbleImageInsets.bottom
+         */
     }
 
 }

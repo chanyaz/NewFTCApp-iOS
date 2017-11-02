045445cf322228abd261868a81101882bf8563eb
Oliver Zhang <[oli.zhang@gmail.com]>
2017-11-02 19:04:39 +0800

    add prefersStatusBarHidden to two places to support both iPhone X and other devices

--- Page/PagesViewController.swift
+++ Page/PagesViewController.swift
@@ -26,7 +26,15 @@ class PagesViewController: UIViewController, UIPageViewControllerDelegate {
         }
     }
     
-
+    // MARK: - This has to be in UInavigation too, otherwise it won't work on iPhone X
+    // Link: - https://forums.developer.apple.com/thread/88962
+    override var prefersStatusBarHidden: Bool {
+        if AppLaunch.sharedInstance.fullScreenDismissed == false {
+            return true
+        } else {
+            return false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

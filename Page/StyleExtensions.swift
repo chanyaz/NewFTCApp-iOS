a6d9c4d0cb063ed19b8d13df61d57e3dab02b0ae
Oliver Zhang <[oli.zhang@gmail.com]>
2017-11-16 13:04:51 +0800

    support night mode

--- Page/StyleExtensions.swift
+++ Page/StyleExtensions.swift
@@ -9,12 +9,6 @@
 import Foundation
 import UIKit
 
-
-
-
-
-
-
 extension UIColor {
     convenience init(hex: String) {
         let hexString = hex.replacingOccurrences(of: "#", with: "")
@@ -33,6 +27,28 @@ extension UIColor {
         )
     }
     
+    convenience init(hex: (day: String, night: String)) {
+        let isNightMode = Setting.isSwitchOn("night-reading-mode")
+        let hexString: String
+        if isNightMode {
+            hexString = hex.night.replacingOccurrences(of: "#", with: "")
+        } else {
+            hexString = hex.day.replacingOccurrences(of: "#", with: "")
+        }
+        let scanner = Scanner(string: hexString)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff,
+            alpha: 1
+        )
+    }
     
     convenience init(hex: String, alpha: CGFloat) {
         let hexString = hex.replacingOccurrences(of: "#", with: "")

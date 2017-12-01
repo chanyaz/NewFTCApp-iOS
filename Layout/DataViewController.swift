c76ef147abe90efa6bc3dbc0244ec5e718fd4082
dreamernumber1 <hhy1770547592@163.com>
2017-12-01 15:01:53 +0800

    fix column

--- Layout/DataViewController.swift
+++ Layout/DataViewController.swift
@@ -44,4 +44,28 @@ class DataViewController: SuperDataViewController,UIGestureRecognizerDelegate{
             }
         }
     }
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return handleItemSelect(indexPath)
+    }
+    
+    // MARK: - Move the handle cell selection to a function so that it can be used in different cases
+    fileprivate func handleItemSelect(_ indexPath: IndexPath) -> Bool {
+        // MARK: Check the fetchResults to make sure there's no out-of-range error
+//        if fetches.fetchResults.count <= indexPath.section || fetches.fetchResults.count == 0 || indexPath.section < 0 {
+//            Track.event(category: "CatchError", action: "Out of Range", label: "handleItemSelect 1")
+//            print ("There is not enough sections in fetchResults")
+//            return false
+//        }
+//        if fetches.fetchResults[indexPath.section].items.count <= indexPath.row || fetches.fetchResults[indexPath.section].items.count == 0 || indexPath.row < 0 {
+//            Track.event(category: "CatchError", action: "Out of Range", label: "handleItemSelect 2")
+//            print ("Row is \(indexPath.row). There is not enough rows in fetchResults Section")
+//            return false
+//        }
+//        let selectedItem = fetches.fetchResults[indexPath.section].items[indexPath.row]
+        if layoutStrategy == "Icons"{
+            return false
+        }
+        return false
+    }
 }
+

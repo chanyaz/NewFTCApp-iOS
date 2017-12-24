0dc9515499f79573a2ee5521d0f4acc5778b0520
Oliver Zhang <oli.zhang@gmail.com>
2017-12-24 22:47:26 +0800

    to do list for editor choice

--- Page/SuperDataViewController.swift
+++ Page/SuperDataViewController.swift
@@ -1158,6 +1158,7 @@ class SuperDataViewController: UICollectionViewController, UINavigationControlle
                     let pageDataRaw = pageData1
                     let pageData: [ContentItem]
                     
+                    // TODO: Editor's Choice Data Can be Transferred
                     if selectedItem.type == "manual" || APIs.shouldHideAd(dataObject) == true {
                         // MARK: For manual html pages in ebooks, hide bottom bar and ads
                         let pageData1 = AdLayout.removeAds(in: pageDataRaw)

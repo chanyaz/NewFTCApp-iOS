33b20a5192c999b7260735ac35a495e7b035a957
Oliver Zhang <oli.zhang@gmail.com>
2017-12-26 19:48:53 +0800

    debug search bar view

--- Page/Main/CustomNavigationController.swift
+++ Page/Main/CustomNavigationController.swift
@@ -43,7 +43,7 @@ class CustomNavigationController: UINavigationController, UINavigationController
     }
     
     // MARK: - This has to be here, otherwise it won't work on iPhone X
-    // Link: - https://forums.developer.apple.com/thread/88962
+    // MARK: - https://forums.developer.apple.com/thread/88962
     override var prefersStatusBarHidden: Bool {
         if AppLaunch.sharedInstance.fullScreenDismissed == false {
             return true
@@ -62,88 +62,6 @@ class CustomNavigationController: UINavigationController, UINavigationController
         }
         return UIStatusBarStyle.default
     }
-    
-    
-    /*
-     
-     // MARK: - https://stackoverflow.com/questions/28949537/uipageviewcontroller-detecting-pan-gestures
-     // MARK: Test custom popping
-     var interactivePopTransition: UIPercentDrivenInteractiveTransition!
-     //var popRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanRecognizer))
-     
-     override func viewDidLoad() {
-     super.viewDidLoad()
-     let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handler))
-     gestureRecognizer.delegate = self
-     view.addGestureRecognizer(gestureRecognizer)
-     }
-     
-     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-     print ("the operation is \(operation)")
-     if (operation == .pop) {
-     print ("the operation is pop")
-     return CustomPopTransition()
-     } else {
-     print ("the operation is not pop")
-     return nil
-     }
-     }
-     
-     func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-     print ("check the animationController type")
-     if animationController is CustomPopTransition {
-     print ("animationController is custom pop transition")
-     return interactivePopTransition
-     } else {
-     print ("animationController is not custom pop transition")
-     return nil
-     }
-     }
-     
-     
-     
-     
-     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-     
-     return true
-     }
-     
-     // MARK: This is working!
-     func handler(_ recognizer: UIPanGestureRecognizer) {
-     let totalTranslation = recognizer.translation(in: view)
-     var progress = recognizer.translation(in: self.view).x / self.view.bounds.size.width
-     progress = min(1, max(0, progress))
-     
-     if (recognizer.state == .began) {
-     // Create a interactive transition and pop the view controller
-     
-     print ("current recognizer state is .began")
-     return
-     self.interactivePopTransition = UIPercentDrivenInteractiveTransition()
-     self.popViewController(animated: true)
-     } else if (recognizer.state == .changed) {
-     // Update the interactive transition's progress
-     print ("current recognizer state is .changed")
-     return
-     interactivePopTransition.update(progress)
-     } else if (recognizer.state == .ended || recognizer.state == .cancelled) {
-     // Finish or cancel the interactive transition
-     print ("current recognizer state is .ended or .cancelled")
-     return
-     
-     if (progress > 0.5) {
-     interactivePopTransition.finish()
-     }
-     else {
-     interactivePopTransition.cancel()
-     }
-     interactivePopTransition = nil
-     }
-     }
-     
-     // MARK: test custom popping end
-     
-     */
     
 }
 
--- Page/SuperDataViewController.swift
+++ Page/SuperDataViewController.swift
@@ -179,16 +179,35 @@ class SuperDataViewController: UICollectionViewController, UINavigationControlle
             webView?.scrollView.bounces = true
             refreshControl.addTarget(self, action: #selector(refreshWebView(_:)), for: UIControlEvents.valueChanged)
             webView?.scrollView.addSubview(refreshControl)
-            
             if dataObjectType == "Search" {
                 searchBar = UISearchBar()
                 searchBar?.sizeToFit()
                 searchBar?.showsScopeBar = true
-                navigationItem.titleView = searchBar
+                //navigationItem.titleView = searchBar
+                
+                
+                
+                // MARK: Use a UIView to contain the Search Bar so that the height won't be larger than needed
+                let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 500))
+                containerView.autoresizesSubviews = true
+                containerView.backgroundColor = UIColor.blue
+                if let searchBar = searchBar {
+                    containerView.addSubview(searchBar)
+//                    searchBar.translatesAutoresizingMaskIntoConstraints = false
+//                    view.addConstraint(NSLayoutConstraint(item: searchBar, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: containerView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0))
+//                    view.addConstraint(NSLayoutConstraint(item: searchBar, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: containerView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0))
+//                    view.addConstraint(NSLayoutConstraint(item: searchBar, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: containerView, attribute: NSLayoutAttribute.left, multiplier: 1, constant: 0))
+//                    view.addConstraint(NSLayoutConstraint(item: searchBar, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: containerView, attribute: NSLayoutAttribute.right, multiplier: 1, constant: 0))
+                }
+                navigationItem.titleView = containerView
+                
+                
+                
                 searchBar?.becomeFirstResponder()
                 searchBar?.delegate = self
+                
+                
                 let urlStringSearch = APIs.convert(APIs.searchUrl)
-                //let urlStringSearch = APIs.searchUrl
                 if let url = URL(string: urlStringSearch) {
                     let request = URLRequest(url: url)
                     let fileName = GB2Big5.convertHTMLFileName("search")

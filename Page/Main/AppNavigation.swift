f5f2452bc7ca3ff4a799e1c914fdcf68caebadd1
Oliver Zhang <[oli.zhang@gmail.com]>
2018-01-22 16:57:32 +0800

    show test page

--- Page/Main/AppNavigation.swift
+++ Page/Main/AppNavigation.swift
@@ -52,6 +52,17 @@ struct AppNavigation {
                     "coverTheme": ""
                 ],
 
+                [
+                    "title": "测试首页",
+                    //"api": "https://api.ftmailbox.com/channel/json.html?pageid=ev&dfadfadfadfadf",
+                    "listapi": "https://api.ftmailbox.com/m/corp/preview.html?pageid=home2&webview=ftcapp&bodyonly=yes&showEnglishAudio=yes&018",
+                    "url":"http://www.ftchinese.com/m/corp/preview.html?pageid=home2",
+                    "screenName":"homepage/editorchoice",
+                    "coverTheme": ""
+                ],
+                
+                
+
 //                [
 //                    "title": "新测试",
 //                    "api":"https://d37m993yiqhccr.cloudfront.net/index.php/jsapi/publish/test",
